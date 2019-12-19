fa6884184b8e5ef65a61739d82e8f17405cf278e
unknown <tierney_mellen@yahoo.com>
2019-12-19 10:15:38 -0500

    first commit, I think I'm done

--- src/main/java/com/github/curriculeon/MyObject.java
+++ src/main/java/com/github/curriculeon/MyObject.java
@@ -3,5 +3,11 @@ package com.github.curriculeon;
 public class MyObject implements Runnable {
     public void run() {
         // TODO
+
+
+    }
+    public String toString()
+    {
+        return "This is an object";
     }
 }
--- src/main/java/com/github/curriculeon/Person.java
+++ src/main/java/com/github/curriculeon/Person.java
@@ -4,22 +4,34 @@ package com.github.curriculeon;
  * Created by leon on 12/16/2019.
  */
 public class Person {
-    public Person(){}
+    private String fName;
+    private String lName;
+
+    public Person(){
+        this.fName = "";
+        this.lName = "";
+    }
 
     public Person(String firstName, String lastName) {
+        this.fName = firstName;
+        this.lName = lastName;
     }
 
     public String getFirstName() {
-        return null;
+
+        return fName;
     }
 
     public void setFirstName(String firstName) {
+        this.fName = firstName;
     }
 
     public String getLastName() {
-        return null;
+
+        return lName;
     }
 
     public void setLastName(String lastName) {
+        this.lName = lastName;
     }
 }
